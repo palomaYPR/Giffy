32de0d76451d666b03fef21b522a86edb76a0d1d
Paloma <yppr09@gmail.com>
2020-10-05 12:17:19 -0500

    ''

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import './App.css'
-import ListOfGifs from './components/ListOfGifs'
-import {Link, Route} from 'wouter'
+import React from "react";
+import "./App.css";
+import ListOfGifs from "./components/ListOfGifs";
+import { Link, Route } from "wouter";
 
-function App() {  
-
+function App() {
   return (
     <div className="App">
       <section className="App-content">
         <h1>Giffy</h1>
-        <Link to='/gif/desencanto'>Des-encanto Netflix Gifs</Link>
-        <Link to='/gif/nation Z'>Nation Z Netflix Gifs</Link>
-        <Link to='/gif/castlevania'>Castlevania Netflix Gifs</Link>
-        <Route 
-          component={ListOfGifs}
-          path="/gif/:keyword"
+        <Link to="/">
+          <img className="App-logo" alt='Giffy logo' src='/logo.png'/>          
+        </Link>
+        <Route
+          component={Home}
+          path='/'
+        />
+        <Route
+          component={SearchResults}        
+          path='/search/:keyword'
+        />
+        <Route
+          component={Detail}
+          path="/gif/:id"
         />        
-      </section>             
+      </section>
     </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { Link } from 'wouter'
 
-export default funtion Detail ({ params }) {
-    console.log(params.id)
-    return <h1> GIF con id { params.id }</h1>
+const POPULAR_GIFS = ['Desencanto', 'Nation Z', 'Castlevania']
+
+export default function Home() {
+    return(
+        <React.Fragment>
+            <h3 className="App-title"> Los Gifs más populares</h3>
+            <ul>
+                {POPULAR_GIFS.map((popularGif) => (
+                    <li key={popularGif}>
+                        <Link to={`/search/${popularGif}`}>Gifs de { popularGif }</Link>
+                    </li>
+                ))}
+            </ul>
+        </React.Fragment>
+    )
 }
