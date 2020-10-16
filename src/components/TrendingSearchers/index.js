190a60ee42673cc72ba86ae3dccb8ad14175a8b2
Paloma <yppr09@gmail.com>
2020-10-15 20:23:52 -0500

    React.Suspense was added to TrendingSearchers

--- src/components/TrendingSearchers/index.js
+++ src/components/TrendingSearchers/index.js
@@ -1,51 +1,14 @@
-import React, {useState, useEffect, useRef} from 'react'
-import getTrendingTerms from 'services/getTrendingTermsService'
-import Category from '../Category'
+import React, {Suspense} from 'react'
+import useNearScreen from 'hooks/useNearScreen'
 
-function TrendingSearches(){
-    const [trends, setTrends] = useState([])
-    useEffect(function (){
-        getTrendingTerms().then(setTrends)
-    }, [])
-    return <Category name='Tendencias' options={trends} />
-}
+const TrendingSearches = React.lazy(() => import('./TrendingSearchers'))
 
-function useNearScreen ( {elementRef} ) {
-    const [isNearScreen, setShow] = useState(false)
+export default function LazyTrending () {        
+    const {isNearScreen, fromRef} = useNearScreen({distance: '200px'})
 
-    useEffect(function (){
-        let observer
-        const onChange = (entries, observer) => {
-            const el = entries[0]
-            if (el.isIntersecting) {
-                setShow(true)
-                observer.disconnect()
-            }
-        }
-
-        Promise.resolve(
-            typeof IntersectionObserver != 'undefined'
-                ? IntersectionObserver
-                : import('intersection-observer')
-        ).then(() => {
-            observer = new IntersectionObserver(onChange, {
-                rootMargin: '100px'
-            })
-    
-            observer.observe(elementRef.current)
-        })           
-
-        return () => observer && observer.disconnect()
-    })
-
-    return isNearScreen
-}
-
-export default function LazyTrending () {    
-    const elementRef = useRef()
-    const isNearScreen = useNearScreen({elementRef})
-
-    return <div ref={elementRef}>
+    return <div ref={fromRef}>
+        <Suspense fallback={null}>
         { isNearScreen ? <TrendingSearches /> : null }
+        </Suspense>
     </div>
 }
