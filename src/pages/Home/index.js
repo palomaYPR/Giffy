a2f57e71e03cb41f8ce8ee4feade841bf3910005
Paloma <yppr09@gmail.com>
2020-10-22 22:45:28 -0500

    Optimization: React.memo was added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useCallback} from 'react'
 import { useLocation } from 'wouter'
 import ListOfGifs from 'components/ListOfGifs/index'
 import { useGifs } from 'hooks/useGifs'
@@ -9,14 +9,14 @@ export default function Home() {
     const [path, pushLocation] = useLocation()
     const { loading, gifs } = useGifs()    
 
-    const handleSubmit = ({keyword}) => {
+    const handleSubmit = useCallback(( {keyword} ) => {
         // navegar a otra ruta        
         pushLocation(`/search/${keyword}`)
-    }
+    }, [pushLocation])    
 
     return(
         <React.Fragment>
-            <SearchForm onSubmit = { handleSubmit }/>
+            <SearchForm onSubmit={ handleSubmit }/>
             <div className='App-main'>
                 <div className='App-results'>
                     <h3 className='App-title'>Última búsqueda</h3>
