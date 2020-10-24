353dee2e7800cc699e58494818a052ab86916945
Paloma <yppr09@gmail.com>
2020-10-23 21:41:48 -0500

    ListOfGifs, React.memo was added (props)

--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -5,12 +5,13 @@ import './ListOfGifs.css'
 export default function ListOfGifs ({ gifs }) {
     return <div className='ListOfGifs'>
         {
-            gifs.map(({id, title, url}) => 
+            gifs.map(({id, title, url, ...restOfGifs}) => 
             <Gif 
                 id = {id}    
                 key = {id}          
                 title = {title}  
-                url = {url}           
+                url = {url} 
+                extraInfo = {restOfGifs}          
             />       
         ) 
         }
